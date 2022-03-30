741143c23020a5e4c21c8e59c598b4aec9ce63ae
Mateus Andre <mateusandresilva2@gmail.com>
2022-03-29 22:10:22 -0300

    test 1 fix heroku

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -2,23 +2,20 @@
     <div class="main">
         <h1>This page is a test</h1>
         <p>Test of vuejs</p>
-
         <p>This part was writed with script: {{ message }}</p>
-        
     </div>
 </template>
 
 <script>
 export default {
-  name: "FirstTest",
-  data(){
+  name: 'FirstTest',
+  data () {
     return {
-      message: "Here is Scripted"
+      message: 'Here is Scripted'
     }
   }
 }
 </script>
 
 <style scoped>
-
 </style>
