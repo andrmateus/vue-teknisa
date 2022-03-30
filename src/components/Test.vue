dc0b188e39ae03aa302381182e4fb546af199746
Mateus Andre <mateusandresilva2@gmail.com>
2022-03-29 21:48:08 -0300

    First Commit

---
+++ src/components/Test.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="main">
+        <h1>This page is a test</h1>
+        <p>Test of vuejs</p>
+
+        <p>This part was writed with script: {{ message }}</p>
+        
+    </div>
+</template>
+
+<script>
+export default {
+  name: "FirstTest",
+  data(){
+    return {
+      message: "Here is Scripted"
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
